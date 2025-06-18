7d3bfad8f2bf75fe08fe6a4f65cdf6c814243a5c
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-06-17 21:34:38 -0300

    feat(frontend): adicionar loader para obter registros do backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ButtonPage from "./pages/ButtonPage";
-import EntriesPage from "./pages/EntriesPage";
+import EntriesPage, { loader as entriesLoader } from "./pages/EntriesPage";
 
 const router = createBrowserRouter([
   { path: "/", Component: ButtonPage },
-  { path: "/registros", Component: EntriesPage },
+  {
+    path: "/registros",
+    Component: EntriesPage,
+    loader: entriesLoader,
+  },
 ]);
 
 export default function App() {
